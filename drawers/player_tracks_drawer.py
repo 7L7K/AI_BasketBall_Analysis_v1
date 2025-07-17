96ffc9e2cdf5663b0b1ec2be3a80c2eb7b4f44f3
Hana Feki <hana.feki@ensta.fr>
2025-07-17 13:08:31 +0200

    Update player_tracks_drawer.py

--- drawers/player_tracks_drawer.py
+++ drawers/player_tracks_drawer.py
@@ -9,10 +9,13 @@ from .utils import draw_ellipse
 
 
 class PlayerTracksDrawer:
-    def __init__(self):
-        pass
+    def __init__(self, team1_color=[255,245,238],team2_color=[128,0,0]):
+        self.default_player_team_id=1
+        self.team1_color=team1_color
+        self.team2_color=team2_color
+        
 
-    def draw(self, video_frames, tracks):
+    def draw(self, video_frames, tracks, player_assignment):
         """
         Draws ellipses on player positions for each frame.
         """
@@ -22,9 +25,15 @@ class PlayerTracksDrawer:
             frame = frame.copy()
             player_dict = tracks[frame_num]
 
+            player_assignment_for_frame=player_assignment[frame_num]
             for track_id, player in player_dict.items():
+                team_id=player_assignment_for_frame.get(track_id,self.default_player_team_id)
+                if team_id ==1:
+                    color=self.team1_color
+                else:
+                    color=self.team2_color
                 bbox = player['box']
-                frame = draw_ellipse(frame, bbox, color=(0, 0, 255))  # Red ellipse
+                frame = draw_ellipse(frame, bbox, color=color) 
 
             output_video_frames.append(frame)
 
