8c657112bf6e52c2a9c9452f3f80d8d099095465
Hana Feki <hana.feki@ensta.fr>
2025-07-17 14:22:42 +0200

    Update bbox_utils.py

--- utils/bbox_utils.py
+++ utils/bbox_utils.py
@@ -2,12 +2,15 @@
 bbox_utils.py
 
 Utility functions for working with bounding boxes.
+
 """
 
+from typing import Tuple
 
-def get_center_bbox(bbox):
+
+def get_center_bbox(bbox: Tuple[float, float, float, float]) -> Tuple[int, int]:
     """
-    Compute the center point of a bounding box.
+    Compute the center (x, y) of a bounding box.
     """
     x1, y1, x2, y2 = bbox
     x_center = int((x1 + x2) / 2)
@@ -15,9 +18,18 @@ def get_center_bbox(bbox):
     return x_center, y_center
 
 
-def get_width_bbox(bbox):
+def get_width_bbox(bbox: Tuple[float, float, float, float]) -> float:
     """
     Compute the width of a bounding box.
     """
     x1, _, x2, _ = bbox
     return x2 - x1
+
+
+def measure_distance(p1: Tuple[float, float], p2: Tuple[float, float]) -> float:
+    """
+    Compute Euclidean distance between two (x, y) points.
+    """
+    x1, y1 = p1
+    x2, y2 = p2
+    return ((x1 - x2)**2 + (y1 - y2)**2)**0.5
