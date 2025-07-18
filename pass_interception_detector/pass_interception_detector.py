d70a11ad6b732c364e04c785425d6931bd0c58a5
Hana Feki <hana.feki@ensta.fr>
2025-07-18 16:57:04 +0200

    Create pass_interception_detector.py

---
+++ pass_interception_detector/pass_interception_detector.py
@@ -0,0 +1,8 @@
+class PassAndInterceptionDetector:
+  def __init__(self):
+    pass
+    
+  def detect_passes(self, ball_acquisition, player_assignment):
+    passes=[-1]*len(ball_acquisition)
+    
+    
