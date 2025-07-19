782781407f9b5bc5ac194a316edd90351627ddbb
Hana Feki <hana.feki@ensta.fr>
2025-07-19 13:48:16 +0200

    Update tactical_view.py

--- tactical_view/tactical_view.py
+++ tactical_view/tactical_view.py
@@ -1,7 +1,13 @@
 import cv2
 class TacticalView:
-  def __init__(self):
-    pass
+  def __init__(self,court_image_path):
+    self.court_image_path=court_image_path
+    self.width=300
+    self.height= 161
+    self.actual_width=28
+    self.actual_height=15
+    
 
   def mapping(self, court_keypoints):
+    
     
