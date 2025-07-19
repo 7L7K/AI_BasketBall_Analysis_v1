693e57d8b55b9eb94eef9628157a4840f6e20dee
Hana Feki <hana.feki@ensta.fr>
2025-07-19 10:58:29 +0200

    Create pass_interception_drawer.py

---
+++ drawers/pass_interception_drawer.py
@@ -0,0 +1,14 @@
+import cv2
+
+class PassInterceptionDrawer:
+  def __init__(self):
+    pass
+  def draw(self, video_frames, passes, interceptions):
+    output_video_frames=[]
+    for frame_num, frame in enumerate(video_frames):
+      frame_drawn=self.draw_frame(frame,fraem_num, passes, interceptions)
+
+  return video_frames
+
+  def 
+      
