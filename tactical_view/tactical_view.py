0f7c73bf918c4a7c255addbddd819a0ac4bd29ad
Hana Feki <hana.feki@ensta.fr>
2025-07-22 00:16:52 +0200

    Update tactical_view.py

--- tactical_view/tactical_view.py
+++ tactical_view/tactical_view.py
@@ -6,8 +6,22 @@ class TacticalView:
     self.height= 161
     self.actual_width_in_meters=28
     self.actual_height_in_meters=15
-    
+    self.keypoints= [
+      # left edge
+      (0,0),
+      (0,int((0.91/self.actual_height_in_meters)*self.height)),
+      (0,int((5.18/self.actual_height_in_meters)*self.height)),
+      (0,int((10/self.actual_height_in_meters)*self.height)),
+      (0,int((14.1/self.actual_height_in_meters)*self.height)),
+      (0,int(self.height)),
 
-  def mapping(self, court_keypoints):
-    
+      # Middle line
+      (int(self.width/2),self.height),
+      (int(self.width/2),0),
+
+      # Left Free throw line
+      (int((5.79/self.actual_width_in_meters)*self.width),int((5.18/self.actual_height_in_meters)*self.height)),
+      (int((5.79/self.actual_width_in_meters)*self.width),int((10/self.actual_height_in_meters)*self.height)),
+
+    ]    
     
